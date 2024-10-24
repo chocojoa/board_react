b25504332d9cf1a82865b8b8ab869063fdfce536
pcj1004 <pcj1004@gmail.com>
2024-10-24 09:42:20 +0900

    fix: menu

--- src/components/layouts/Nav.jsx
+++ src/components/layouts/Nav.jsx
@@ -1,9 +1,8 @@
-import { forwardRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { Smile } from "lucide-react";
 
-import { cn } from "@/lib/utils";
 import authSlice from "@/store/authSlice";
 import useAxios from "@/hooks/useAxios";
 
@@ -14,13 +13,12 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "../ui/navigation-menu";
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "../ui/menubar";
 
 const Nav = () => {
   const api = useAxios();
@@ -49,22 +47,23 @@ const Nav = () => {
       url: "/api/admin/menus",
       method: "GET",
     }).then((response) => {
+      console.log(response);
       setMenu(response.data.data);
     });
   };
 
-  const getNavigationMenuContent = (menuId) => {
+  const getMenuContent = (menuId) => {
     const childMenu = menu.filter(
       (e) => e.groupId === menuId && e.menuId !== menuId
     );
     return (
-      <ul className="grid gap-3 p-2 md:w-[150px] lg:w-[150px]">
-        {childMenu.map((e) => (
-          <ListItem key={e.menuId} to={e.menuUrl}>
-            {e.menuName}
-          </ListItem>
+      <MenubarContent>
+        {childMenu.map((m) => (
+          <Link to={m.menuUrl} key={m.menuId}>
+            <MenubarItem>{m.menuName}</MenubarItem>
+          </Link>
         ))}
-      </ul>
+      </MenubarContent>
     );
   };
 
@@ -82,30 +81,26 @@ const Nav = () => {
                 <Link to="/">React 연습화면</Link>
               </div>
               <div className="flex flex-shrink-0 items-center space-x-6 mx-10 text-sm/[17px]">
-                <NavigationMenu>
-                  <NavigationMenuList>
-                    {menu.map((e) => {
-                      if (!e.parentMenuId && e.childCount == 0) {
-                        return (
-                          <NavigationMenuItem key={e.menuId}>
-                            <Link to={e.menuUrl}>{e.menuName}</Link>
-                          </NavigationMenuItem>
-                        );
-                      } else if (!e.parentMenuId && e.childCount > 0) {
-                        return (
-                          <NavigationMenuItem key={e.menuId}>
-                            <NavigationMenuTrigger>
-                              {e.menuName}
-                            </NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                              {getNavigationMenuContent(e.menuId)}
-                            </NavigationMenuContent>
-                          </NavigationMenuItem>
-                        );
-                      }
-                    })}
-                  </NavigationMenuList>
-                </NavigationMenu>
+                <Menubar className="border-none shadow-none">
+                  {menu.map((m) => {
+                    if (m.depth === 1 && m.childCount > 0) {
+                      return (
+                        <MenubarMenu key={m.menuId}>
+                          <MenubarTrigger>{m.menuName}</MenubarTrigger>
+                          {getMenuContent(m.menuId)}
+                        </MenubarMenu>
+                      );
+                    } else if (m.depth === 1 && m.childCount === 0) {
+                      return (
+                        <MenubarMenu key={m.menuId}>
+                          <Link to={m.menuUrl}>
+                            <MenubarTrigger>{m.menuName}</MenubarTrigger>
+                          </Link>
+                        </MenubarMenu>
+                      );
+                    }
+                  })}
+                </Menubar>
               </div>
             </div>
             <div className="flex-shrink-0 text-sm">
@@ -134,29 +129,5 @@ const Nav = () => {
     </>
   );
 };
-
-const ListItem = forwardRef(({ className, to, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <Link
-          to={to}
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </Link>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-
-ListItem.displayName = "ListItem";
 
 export default Nav;
