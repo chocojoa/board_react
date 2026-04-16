c9050d6f96d69009c5c460d654a1bb6f7fb52a11
pcj1004 <pcj1004@gmail.com>
2026-04-16 14:58:58 +0900

    feat: initialize Redux store and update project dependencies to latest versions

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
+import storage from "redux-persist/es/storage";
 import { persistStore, persistReducer } from "redux-persist";
 import {
   FLUSH,
